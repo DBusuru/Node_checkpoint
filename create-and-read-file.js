3fe6af96b392d139ee2c4b4e8d9aa17e8420ae13
David Busuru <davidbusuru1@gmail.com>
2025-05-13 20:51:27 +0300

    Node

---
+++ create-and-read-file.js
@@ -0,0 +1,11 @@
+// create-and-read-file.js
+const fs = require('fs');
+
+// Create a file with "Hello Node"
+fs.writeFileSync('welcome.txt', 'Hello Node');
+
+// Read and log the content of the file
+fs.readFile('welcome.txt', 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
---
+++ hello-world.js
@@ -0,0 +1,3 @@
+// hello-world.js
+console.log("HELLO WORLD");
+// This is a simple JavaScript program that prints "HELLO WORLD" to the console.
